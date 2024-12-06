8ab1082fa2f7245f717768299e85ab84ca421b05
mohammed lahmamsi <Lahmamsi.med@gmail.com>
2024-12-07 00:43:13 +0100

    Fixed authentication by moving middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
-// Job routes
-Route::resource('jobs', JobController::class);
+// Public job routes
+Route::get('/jobs', [JobController::class, 'index'])->name('jobs.index');
+
+// Protected job routes
+Route::middleware('auth')->group(function () {
+    Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
+    Route::post('/jobs', [JobController::class, 'store'])->name('jobs.store');
+    Route::get('/jobs/{job}', [JobController::class, 'show'])->name('jobs.show');
+    Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->name('jobs.edit');
+    Route::put('/jobs/{job}', [JobController::class, 'update'])->name('jobs.update');
+    Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->name('jobs.destroy');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
